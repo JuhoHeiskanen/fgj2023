b624f2abdd55072a9ef517792d7ae35b44ae4257
Juho Heiskanen <juho.heiskanen@gmail.com>
2023-02-05 11:23:13 +0200

    feat: monsters go after resource spawners

--- Scripts/NavigationTest.gd
+++ Scripts/NavigationTest.gd
@@ -8,6 +8,7 @@ export var melee_cooldown: float = 0.0
 export var melee_delay: float = 2.5
 export var attack_windup: float = 1.0
 var attack_timer: float = 0.0
+export var goes_after_resource_spawners: bool = true
 
 onready var player = $"../Player"
 onready var navigation: NavigationAgent = $"NavigationAgent"
@@ -25,25 +26,37 @@ func _ready():
 	NavigationServer.set_active(true)
 
 func _physics_process(delta):
+	var targets = get_tree().get_nodes_in_group("monster-target" if goes_after_resource_spawners else "player")
+	if targets.size() <= 0:
+		return
+	var smallest_distance = (targets[0].translation - self.translation).length_squared()
+	var target_entity = targets[0]
+	for t in targets:
+		var dist = (t.translation - self.translation).length_squared()
+		if dist < smallest_distance:
+			smallest_distance = dist
+			target_entity = t
+	
 	var attacking = false
 	if attack_timer > 0:
 		attack_timer -= delta
 		if attack_timer <= 0:
-			var diff_to_player: Vector3 = player.translation - self.translation
-			if diff_to_player.length_squared() <= melee_range * melee_range:
+			var diff_to_target: Vector3 = target_entity.translation - self.translation
+			if diff_to_target.length_squared() <= melee_range * melee_range:
 				melee_cooldown = melee_delay
-				player.hurt(self.melee_damage)
+				if target_entity.has_method("hurt"):
+					target_entity.hurt(self.melee_damage)
 		attacking = true
 	elif melee_cooldown <= 0:
-		var diff_to_player: Vector3 = player.translation - self.translation
-		if diff_to_player.length_squared() <= melee_range * melee_range:
+		var diff_to_target: Vector3 = target_entity.translation - self.translation
+		if diff_to_target.length_squared() <= melee_range * melee_range:
 			attack_timer = attack_windup
 	else:
 		melee_cooldown -= delta
 
 	if !attacking:
 		navigation.set_max_speed(speed)
-		navigation.set_target_location(player.translation)
+		navigation.set_target_location(target_entity.translation)
 		var next_location = navigation.get_next_location()
 		var diff = next_location - self.translation
 		diff = diff.normalized() * speed
--- Scripts/ResourceSpawner.gd
+++ Scripts/ResourceSpawner.gd
@@ -3,16 +3,20 @@ extends StaticBody
 var erected = false
 var resource_kind: int
 var hp: int = 0
+export var max_hp: int = 5
 
 func hurt(damage: int):
 	if erected:
 		hp -= damage
 		if hp <= 0:
 			visible = false
+			self.remove_from_group("monster-target")
 		return
 
 	$Shroom.visible = true
 	erected = true
+	hp = max_hp
+	self.add_to_group("monster-target")
 
 func _physics_process(delta: float) -> void:
 	match resource_kind:
